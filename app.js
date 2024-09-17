b1b680875886c5839bc409184694d5f08a791ff4
Mohammad Allam <uodeeb@gmail.com>
2024-09-17 15:14:01 +0300

    dynamic navigation quiz

---
+++ app.js
@@ -0,0 +1,21 @@
+/*-------------------------dynamic navigation---------------
+* Description:
+when adding a new section in HTML page, a new navigation 
+item should be generated using javascript.
+* Inputs:
+    - container nav__list
+    - section list 
+* Outputs:
+    dynamic navigation items <li></li>
+    anchor tags <a></a>
+* Logic:
+    1. store container in a variable.
+    2. store section list in a variable.
+    3. loop through section list.
+    4. for each section, create a <li> <a> tag.
+    5. add <a>text</a>
+    6. add href attribute
+    7. append <a> to <li>
+    8. append <li> to container
+*/
+
